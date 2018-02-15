b9ebf448ce761f86fe713e9128654c21d7cdc082
Veremey <veremey4uk@gmail.com>
2018-02-15 14:53:43 +0200

    #1

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -5742,66 +5742,59 @@ $(document).ready(function () {
   tenpeoples = 0,
   hungredpeoples = 0,
   thouthendpeoples = 0;
+  var $height;
 
-  setInterval(
-    function () {
-    	peoples += (90 / 60);
-        $('.peoples').css({'top': -peoples});
-  }, 1000);
+  if($(window).width() < 764){
+  	$height = 72;
+  } else if ($(window).width() < 484) {
+  	$height = 44;
+  } else {
+  	$height = 88;
+  }
 
-  // var ee = setInterval( timer, 1000);
-
+  var onePeopleUp = setInterval( timer, 1000);
 
   setInterval(function () {
   	peoples = 0;
+  	tenpeoples += $height;
+  	$('.peoples').css({'top': -peoples});
+  	$('.tenpeoples').css({'top': -tenpeoples});
   }, 600000);
 
+  function timer() {
+    peoples +=(90 / 60);
+    $('.peoples').css({'top': -peoples});
+    return peoples;
+  }
 
-  setInterval(
-    function () {
-    	tenpeoples +=(90 / 60);
-       $('.tenpeoples').css({'top': -tenpeoples});
-      return tenpeoples;
-	  }, 10000);
+  setInterval(function () {
+  	tenpeoples = 0;
+  	peoples = 0;
+  	hungredpeoples += $height;
+  	$('.peoples').css({'top': -peoples});
+  	$('.tenpeoples').css({'top': -tenpeoples});
+  	$('.hungredpeoples').css({'top': -hungredpeoples});
+  }, 6000000);
 
+  setInterval(function () {
+  	tenpeoples = 0;
+  	peoples = 0;
+  	hungredpeoples = 0;
+  	thouthendpeoples += $height;
+  	$('.peoples').css({'top': -peoples});
+  	$('.tenpeoples').css({'top': -tenpeoples});
+  	$('.hungredpeoples').css({'top': -hungredpeoples});
+  	$('.thouthendpeoples').css({'top': -thouthendpeoples});
+  }, 600000000);
 
-  // experiment
-  // function timer() {
-  // 	console.log('peoples - ' + peoples);
-  // 	if (peoples > 10 ) {
-  // 		tenpeoples = 99;
-  // 	}
-  //   peoples +=(90 / 60);
-  //   $('.peoples').css({'top': -peoples});
-  //   return peoples;
-  // }
-  // experiment
+  // setInterval(function () {
+  //   thouthendpeoples +=(90 / 60);
+  //     $('.thouthendpeoples').css({'top': -thouthendpeoples});
+  // }, 100000000);
 
-
-
-   setInterval(function () {
-   	tenpeoples = 0;
-   }, 6000000);
-
-   window.setInterval(
-    function () {
-    	hungredpeoples +=(90 / 60);
-        $('.hungredpeoples').css({'top': -hungredpeoples});
-    }, 1000000);
-
-   setInterval(function () {
-   	hungredpeoples = 0;
-   }, 600000000);
-
-   window.setInterval(
-    function () {
-    	thouthendpeoples +=(90 / 60);
-        $('.thouthendpeoples').css({'top': -thouthendpeoples});
-    }, 100000000);
-
-   setInterval(function () {
-   	thouthendpeoples = 0;
-   }, 60000000000);
+  // setInterval(function () {
+  //   thouthendpeoples = 0;
+  // }, 60000000000);
 
 
 	});
